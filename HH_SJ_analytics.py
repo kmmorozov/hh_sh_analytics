c86f4c8e9748287fbeae59e4fb60891d8a7be0a6
kmmorozov <kmmorozov@gmail.com>
2022-08-20 22:28:04 +0300

    Убрал лишнее преобразование типов

--- HH_SJ_analytics.py
+++ HH_SJ_analytics.py
@@ -32,11 +32,11 @@ def predict_salary(salary_from, salary_to):
     if not salary_from and not salary_to:
         avg_salary = None
     elif salary_from and salary_to:
-        avg_salary = float(salary_from + salary_to) / 2
+        avg_salary = (salary_from + salary_to) / 2
     elif salary_from:
-        avg_salary = float(salary_from) * 1.2
+        avg_salary = salary_from * 1.2
     elif salary_to:
-        avg_salary = float(salary_to) * 0.8
+        avg_salary = salary_to * 0.8
     return avg_salary
 
 
